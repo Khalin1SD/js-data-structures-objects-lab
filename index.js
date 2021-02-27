03514c8cd6b2d62180690f162f67f060afcfe54c
Khalin Redding <khalinredd@gmail.com>
2021-02-27 13:37:31 -0500

    commit tree 029185313a3c12ed8a457453f54712e2fcac83de

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your solution in this file!
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newObj = { ...driver };
+  
+    newObj[key] = value;
+  
+    return newObj;
+  }
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  
+    driver[key] = value;
+  
+    return driver;
+  }
